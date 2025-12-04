52752fa495f5b983cd3e79025aa47b2c8aff98a5
Gladys <mungaiglado106@gmail.com>
2025-12-04 14:41:36 +0300

    defining methods that take in parameter

--- method.rb
+++ method.rb
@@ -35,5 +35,51 @@ end
 def area
     length = 10
     width = 5
-    
+
 end
+#defining a method with default arguments
+def greeting(name="stranger")
+    "Hello, #{name}!"
+
+end
+puts greeting("Alice")  # output: Hello, Alice!
+puts greeting()         # output: Hello, stranger!
+
+#splat operator is used to accept variable number of arguments
+# we use an asterisk (*) before the parameter name to indicate that it can accept multiple arguments as an array.
+def sum_all(*numbers)
+    numbers.sum
+end
+def sum_all_flexible(*nums)
+    nums= nums.first if nums.length == 1 && nums.first.is_a?(Array)
+    nums.sum
+
+end
+sum_all_flexible(1, 2, 3)          # output: 6
+sum_all_flexible([1, 2, 3, 4, 5])  # output: 15
+sum_all()
+
+
+def extra_curiculars(*clubs)
+    club.each {|club| puts "I am part of the #{club} club."}
+end
+extra_curiculars("Drama", "Science", "Math")
+# output:
+# I am part of the Drama club.
+# I am part of the Science club.
+# I am part of the Math club.
+
+def add(num1,num2)
+    return num1 + num2
+end
+=begin Define a method called greeter that takes a single string parameter, name, and returns a string containing that person’s name.
+
+Make sure to return the string. Don’t use print or puts.
+=end
+
+def greeter(name)
+    return "Hello,#{name}!"
+end
+
+
+
